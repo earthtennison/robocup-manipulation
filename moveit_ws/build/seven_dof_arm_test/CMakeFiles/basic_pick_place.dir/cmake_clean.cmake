a6099489659d171308c8b6d078debf916817f7d3
Famee <fjj20092@gmail.com>
2021-10-29 15:42:06 +0700

    Add moveit_ws

---
+++ moveit_ws/build/seven_dof_arm_test/CMakeFiles/basic_pick_place.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_pick_place.dir/src/pick_place.cpp.o"
+  "/home/famee/Desktop/moveit_ws/devel/lib/seven_dof_arm_test/basic_pick_place.pdb"
+  "/home/famee/Desktop/moveit_ws/devel/lib/seven_dof_arm_test/basic_pick_place"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_pick_place.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ moveit_ws/devel/share/seven_dof_arm_test/cmake/seven_dof_arm_test-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(seven_dof_arm_test_MESSAGE_FILES "")
+set(seven_dof_arm_test_SERVICE_FILES "")
