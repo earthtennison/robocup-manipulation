01a6ed42a6fec24d0c698b5974b7ae1a83b3daee
Nae <ktponthuam@gmail.com>
2021-10-29 16:33:18 +0700

    delete alien folder

--- fame_ws/build/seven_dof_arm_test/CMakeFiles/seven_dof_arm_test_generate_messages_cpp.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/seven_dof_arm_test_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- fame_ws/build/seven_dof_arm_test/catkin_generated/installspace/seven_dof_arm_test-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(seven_dof_arm_test_MESSAGE_FILES "")
-set(seven_dof_arm_test_SERVICE_FILES "")
