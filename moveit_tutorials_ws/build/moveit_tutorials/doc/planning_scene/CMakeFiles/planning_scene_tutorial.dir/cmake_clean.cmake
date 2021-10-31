6dfc051e2b467de055331f0cd6630a9233fe95af
Famee <fjj20092@gmail.com>
2021-10-31 22:35:22 +0700

    Build package

---
+++ moveit_tutorials_ws/build/moveit_tutorials/doc/planning_scene/CMakeFiles/planning_scene_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/planning_scene_tutorial.dir/src/planning_scene_tutorial.cpp.o"
+  "/home/famee/Desktop/robocup-manipulation/moveit_tutorials_ws/devel/lib/moveit_tutorials/planning_scene_tutorial.pdb"
+  "/home/famee/Desktop/robocup-manipulation/moveit_tutorials_ws/devel/lib/moveit_tutorials/planning_scene_tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/planning_scene_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
