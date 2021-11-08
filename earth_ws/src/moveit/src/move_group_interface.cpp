e5eb787ee2238840f13ea3cb69a710c90bc49be7
earthtennison <earth.tennison@gmail.com>
2021-11-08 20:38:48 +0700

    debug move_group_interface.cpp

--- earth_ws/src/moveit/src/move_group_interface.cpp
+++ earth_ws/src/moveit/src/move_group_interface.cpp
@@ -20,57 +20,67 @@ private:
   ros::Publisher pub;
   ros::Subscriber sub;
   ros::NodeHandle node_handle;
-  std::string PLANNING_GROUP = "panda_arm";
-  moveit::planning_interface::MoveGroupInterface* move_group_interface_ptr;
-  moveit_visual_tools::MoveItVisualTools* visual_tools_ptr;
-  moveit::core::JointModelGroup* joint_model_group;
+  geometry_msgs::Pose::ConstPtr goal_pose;
 
 public:
   MyMoveGroupInterface()
   {
   // initialize node
-  namespace rvt = rviz_visual_tools;
-  moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
-  move_group_interface_ptr = &move_group_interface;
-  joint_model_group = move_group_interface_ptr->getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-
+  sub = node_handle.subscribe("object_coor", 10, &MyMoveGroupInterface::object_coor_callback, this);
   
-  moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
-  visual_tools.deleteAllMarkers();
-  visual_tools_ptr = &visual_tools;
-
-  sub = node_handle.subscribe<geometry_msgs::Pose>("object_coor", 10, &MyMoveGroupInterface::object_coor_callback, this);
   }
 
-  void object_coor_callback(const geometry_msgs::Pose::ConstPtr& msg)
+  void object_coor_callback(const geometry_msgs::Pose::ConstPtr msg)
   {
       ROS_INFO("[CV_NODE]: x: [%f] y:[%f] z: [%f]", msg->position.x, msg->position.y, msg->position.z);
       ROS_INFO("[CV_NODE]: qx: [%f] qy:[%f] qz: [%f] qw:[%f]", msg->orientation.x, msg->orientation.y, msg->orientation.z, msg->orientation.w);
-      
-      // set goal pose
-      geometry_msgs::Pose target_pose1;
-      target_pose1.orientation.w = msg->orientation.w;
-      target_pose1.orientation.x = msg->orientation.x;
-      target_pose1.orientation.y = msg->orientation.y;
-      target_pose1.orientation.z = msg->orientation.z;
-      target_pose1.position.x = msg->position.x;
-      target_pose1.position.y = msg->position.y;
-      target_pose1.position.z = msg->position.z;
+      goal_pose = msg;
+      move();
+  }
 
-      move_group_interface_ptr->setPoseTarget(target_pose1);
+  void move()
+  {
+    namespace rvt = rviz_visual_tools;
+    std::string PLANNING_GROUP = "panda_arm";
+    moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
+    const moveit::core::JointModelGroup* joint_model_group = move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-      moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-      bool success = (move_group_interface_ptr->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-      ROS_INFO("Plan %s", success ? "success" : "failure");
 
-      // visualize plan
-      visual_tools_ptr->publishAxisLabeled(target_pose1, "pose 1");
-      visual_tools_ptr->publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-      visual_tools_ptr->trigger();
+    moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
+    visual_tools.deleteAllMarkers();
+    // set goal pose
+    geometry_msgs::Pose target_pose1;
+    target_pose1.orientation.w = goal_pose->orientation.w;
+    target_pose1.orientation.x = goal_pose->orientation.x;
+    target_pose1.orientation.y = goal_pose->orientation.y;
+    target_pose1.orientation.z = goal_pose->orientation.z;
+    target_pose1.position.x = goal_pose->position.x;
+    target_pose1.position.y = goal_pose->position.y;
+    target_pose1.position.z = goal_pose->position.z;
 
-      bool success_execute = (move_group_interface_ptr->execute(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-      ROS_INFO("Plan %s", success_execute ? "success" : "failure");
-      ROS_INFO("Successfully executed!");
+    // target_pose1.orientation.w = 1.0;
+    // target_pose1.position.x = 0.28;
+    // target_pose1.position.y = -0.2;
+    // target_pose1.position.z = 0.5;
+
+    move_group_interface.setPoseTarget(target_pose1);
+    ROS_INFO("target set");
+
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+    ROS_INFO("target set1");
+    bool success = (move_group_interface.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    ROS_INFO("target set2");
+    ROS_INFO("Plan %s", success ? "success" : "failure");
+
+    // visualize plan
+    visual_tools.publishAxisLabeled(target_pose1, "pose 1");
+    visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
+    visual_tools.trigger();
+
+    bool success_execute = (move_group_interface.execute(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+    ROS_INFO("Plan %s", success_execute ? "success" : "failure");
+    ROS_INFO("Successfully executed!");
   }
 
 };
@@ -78,7 +88,7 @@ public:
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "move_group_interface");
-  ros::AsyncSpinner spinner(1);
+  ros::AsyncSpinner spinner(0);
   spinner.start();
   
   
