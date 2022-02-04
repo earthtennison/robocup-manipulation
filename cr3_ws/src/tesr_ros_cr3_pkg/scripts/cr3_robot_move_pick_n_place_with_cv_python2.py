158e06efb3834d1d72f107d951ca0efb1a202ec0
Nae <ktponthuam@gmail.com>
2022-02-04 15:56:55 +0700

    add pick_n_place_with_cv_python2

--- cr3_ws/src/tesr_ros_cr3_pkg/scripts/cr3_robot_move_pick_n_place_with_cv_python2.py
+++ cr3_ws/src/tesr_ros_cr3_pkg/scripts/cr3_robot_move_pick_n_place_with_cv_python2.py
@@ -1,5 +1,4 @@
 from dobot_api import dobot_api_dashboard, dobot_api_feedback, MyType
-import _thread
 import numpy as np
 import socket
 import cv2 as cv
@@ -8,34 +7,6 @@ from custom_socket import CustomSocket
 import json
 
 dobot_Enable = True
-
-def CR3_feedback(threadName):
-    global dobot_Enable,client_feedback
-    while dobot_Enable == True:
-        time.sleep(0.05)
-        all = client_feedback.socket_feedback.recv(10240)
-        data = all[0:1440]
-        a = np.frombuffer(data, dtype=MyType)
-        try:
-            if hex((a['test_value'][0])) == '0x123456789abcdef':
-                print("============== Feed Back ===============")
-                CR3_endpoint =  np.around(a['Tool_vector_target'], decimals=4)[0]
-                print("CR3_endpoint: [x:{0}] , [y:{1}] , [z:{2}] , [rx:{3}] , [ry:{4}] , [rz:{5}]".format(CR3_endpoint[0],CR3_endpoint[1],CR3_endpoint[2],CR3_endpoint[3],CR3_endpoint[4],CR3_endpoint[5]))                            
-                CR3_joint = np.around(a['q_target'], decimals=4)[0]
-                print("CR3_joint: [j1:{0}] , [j2:{1}] , [j3:{2}] , [j4:{3}] , [j5:{4}] , [j6:{5}]".format(CR3_joint[0],CR3_joint[1],CR3_joint[2],CR3_joint[3],CR3_joint[4],CR3_joint[5]))
-                print("========================================")
-
-                CR3_digitalInput =  np.around(a['digital_input_bits'])[0]
-                get_bin = lambda x, n: format(x, 'b').zfill(n)
-                print("MG400_digitalInput: ",get_bin(CR3_digitalInput,16)) # MSB format
-                print("========================================")
-        except:
-            print("Some error with robot.")
-            
-            
-    client_dashboard.DisableRobot()
-    client_feedback.close()
-    print('!!!!!! client_feedback END !!!!!!')
 
 def pick():
         #open gripper
@@ -96,7 +67,6 @@ client_dashboard.Tool(0)
 client_dashboard.DO(2,0)
 client_dashboard.DO(1,1)
 
-_thread.start_new_thread(CR3_feedback, ("Thread-1",))
 
 host = "192.168.10.79"
 port = 10001
