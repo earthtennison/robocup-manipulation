daff708650238afe9ff05d513da4584ef57af399
ChayaAsa <chayaasa@gmail.com>
2022-06-11 19:12:46 +0700

    mill change to defult

--- cr3_ws/src/gb_visual_detection_3d/darknet_ros_3d/src/demo/darknet3d_node.cpp
+++ cr3_ws/src/gb_visual_detection_3d/darknet_ros_3d/src/demo/darknet3d_node.cpp
@@ -38,12 +38,44 @@
 #include <ros/ros.h>
 
 #include "darknet_ros_3d/Darknet3D.h"
+#include "test_service/obj2pose_service.h"
+
+bool requestCb(test_service::obj2pose_service::Request &req,
+                test_service::obj2pose_service::Response &res){
+  if (req.obj2pose_request == true){
+    while (ros::ok())
+    {
+      darknet_ros_3d::Darknet3D darknet3d;
+      darknet3d.update();
+      // ros::spinOnce();
+      res.obj2pose_respond = true;
+      ROS_INFO("Finish Callback request");
+      break;
+
+    }
+  }
+  else{
+    res.obj2pose_respond = false;
+    ROS_ERROR("Something wrong Request is false");
+  }
+  
+
+  return true;
+  
+}
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "darknet_3d");
   darknet_ros_3d::Darknet3D darknet3d;
   ros::Rate loop_rate(10);
+
+  //---edit
+  // ros::NodeHandle nh;
+  // ros::ServiceServer server = nh.advertiseService("obj2pose_service",requestCb);
+  // ros::spin();
+  // ros::waitForShutdown();
+
 
   while (ros::ok())
   {
@@ -53,5 +85,6 @@ int main(int argc, char **argv)
     loop_rate.sleep();
   }
 
+  
   return 0;
 }
