dfbc8a569de7b1aaee50c0f5f406e3985c64d5e8
Pornpipat Oupchan <skarn.pzs35ca@gmail.com>
2022-06-17 18:14:12 +0700

    add pick service with select pick side

---
+++ cr3_ws/src/cr3_moveit_control/src/pick_client_2_python.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Pose
+from tf.transformations import quaternion_from_euler
+import math
+from cr3_moveit_control.srv import *
+
+def to_rad(deg):
+    return deg * math.pi / 180.0
+
+def pick_service(goal_pose, pick_side):
+    rospy.wait_for_service('pick_gen2_success')
+    try:
+        pick = rospy.ServiceProxy('pick_gen2_success', pick_server2)
+        res = pick(goal_pose, pick_side)
+        return res.success_grasp
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+
+if __name__ == "__main__":
+    while not rospy.is_shutdown():
+        x = input("X ")
+        y = input("y ")
+        z = input("z ")
+        row = to_rad(input("row "))
+        pitch = to_rad(input("pitch "))
+        yaw = to_rad(input("yaw "))
+        q = quaternion_from_euler(row, pitch, yaw)
+        pick_side_input = input("side (front, top, left, right)")
+        pose_goal = Pose()
+        pose_goal.orientation.x = q[0]
+        pose_goal.orientation.y = q[1]
+        pose_goal.orientation.z = q[2]
+        pose_goal.orientation.w = q[3]
+
+        pose_goal.position.x = x
+        pose_goal.position.y = y
+        pose_goal.position.z = z
+        pick_side = pick_side_input;
+        #pose_goal.pick_side = pick_side
+        rospy.loginfo(pose_goal, pick_side)
+        success = pick_service(pose_goal, pick_side)
+        print(success)
