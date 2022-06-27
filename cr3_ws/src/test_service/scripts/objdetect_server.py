c53cb96f230af497feff954ae165432f333f1b25
ChayaAsa <chayaasa@gmail.com>
2022-06-27 22:15:20 +0700

    add launch flie for objdetect_server

--- cr3_ws/src/test_service/scripts/objdetect_server.py
+++ cr3_ws/src/test_service/scripts/objdetect_server.py
@@ -1,3 +1,9 @@
+
+"""
+launch file 
+roslaunch realsense2_camera rs_rgbd.launch align_depth:=true color_width:=1280 color_height:=720 color_fps:=30 depth_width:=1280 depth_height:=720 depth_fps:=30 filters:=pointcloud
+"""
+
 #!/usr/bin/env python
 
 import rospy
