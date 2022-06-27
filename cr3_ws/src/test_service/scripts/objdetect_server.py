0ff9c4ea6ffa70c2e17811ebe483ffa80fee79c6
ChayaAsa <chayaasa@gmail.com>
2022-06-27 22:17:03 +0700

    fix launch error

--- cr3_ws/src/test_service/scripts/objdetect_server.py
+++ cr3_ws/src/test_service/scripts/objdetect_server.py
@@ -1,9 +1,3 @@
-
-"""
-launch file 
-roslaunch realsense2_camera rs_rgbd.launch align_depth:=true color_width:=1280 color_height:=720 color_fps:=30 depth_width:=1280 depth_height:=720 depth_fps:=30 filters:=pointcloud
-"""
-
 #!/usr/bin/env python
 
 import rospy
